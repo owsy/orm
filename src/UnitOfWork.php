ef0b2541c386d7fa71e0df9308f7fc1a4ef99dd2
Rhodri Pugh <rod@owsy.com>
2024-06-01 18:21:28 +0100

    chore: add temporary workaround to expose PDO exceptions from commits
    these are caused by postgres constraint issues, and are not currently
    handled well by doctrine:
    
    https://github.com/doctrine/orm/issues/7545

--- src/UnitOfWork.php
+++ src/UnitOfWork.php
@@ -482,7 +482,11 @@ class UnitOfWork implements PropertyChangedListener
             $this->em->close();
 
             if ($conn->isTransactionActive()) {
-                $conn->rollBack();
+                try {
+                    $conn->rollBack();
+                } catch (Exception) {
+                    // Swallow to preserve commit exception.
+                }
             }
 
             $this->afterTransactionRolledBack();
